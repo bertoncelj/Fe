16d72211053041812a8523367f89f16b4f1ad346
Tine Bertoncelj <king.tine@gmail.com>
2017-10-24 00:02:14 +0200

    Last comm 23.10

--- Signali/Vaja2/forloop.m
+++ Signali/Vaja2/forloop.m
@@ -1,14 +1,30 @@
 
+clear all; close all;
+L = 1 / 100;
+C = 1 / 1000000;
 
-L = 10;
-C = 1;
+Real_Imag_m = [];
+Real_m = [];
+Imag_m = [];
+
+rm = 1; im = 1; ri = 1;
+printf("STARTEN!!! \n");
 for R = 1:1000
     en = -R/(2*L) - (sqrt(C* (C*R^2 - 4*L)) / (2 * C * L));
     
 	  if(imag(en) == 0)
-      printf("%d is Real \n", en);
+      %printf("Real: %d on R: %d \n", en, R);
+      Real_m = [Real_m; R, real(en)];
+      
+    elseif (real(en) == 0)
+      Imag_m = [Imag_m; R, imag(en)];
+      
     else
-      printf("%d is Imag\n", en);
+      %printf("Imag: %d %d  on R: %d \n", real(en), imag(en), R);
+      Real_Imag_m = [Real_Imag_m; R, real(en), imag(en)];
+      
     endif
- 
 end
+Real_m
+Real_Imag_m
+Imag_m
