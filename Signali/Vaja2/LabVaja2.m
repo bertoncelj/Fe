3a20de9ab0c9be16708f4277985db292d62e32bc
Tine <tine.bertoncelj@gmail.com>
2017-11-03 13:16:54 +0100

    Started lab vaja2

--- Signali/Vaja2/LabVaja2.m
+++ Signali/Vaja2/LabVaja2.m
@@ -4,3 +4,22 @@ clear all; close all;
 # Program dopolnite, da bo izračunal še napetost na kondenzatorju vC(t) in nato obe spremenljivki 
 #grafično prikazal na podgrafih. (C = 1 mF, L = 110 mH, R1 = 100 Ω, R2 = 10 Ω, iL(0) = 1 A, vC(0) = 0 V)
 
+Lr = vpa ('110 / 1000', 32);     %Henry
+Cr = vpa('1/1000', 32);    %Farad          %ohm
+R1 = 100;
+R2 = 10;
+
+A = (R1 * R2)/(R1 +R2)
+a2 = Lr * Cr
+a1 = (R2 * Cr) - Lr * (A - 1/(R2))
+a0 = -R2* A
+syms il(t)
+
+DE = a2 * diff(il, t, t) + a1 * diff(il, t) + a0 * il == 0
+sol = dsolve (DE, il(0) == 1, diff(il)(0) == 10);
+t1 = 0 : 0.01 : 2;
+
+ff   = function_handle(rhs(sol));
+
+y  = ff(t1);
+plot(t1,y);
