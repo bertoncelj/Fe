e53618234c8584198b0b7a22e133fa2e9f98a0ff
Tine Bertoncelj <king.tine@gmail.com>
2017-11-13 22:08:31 +0100

    Working example

--- Signali/Vaja4/test_lsim.m
+++ Signali/Vaja4/test_lsim.m
@@ -1,23 +1,54 @@
-R1 = 2; R2 = 1; C1 = 1; C2 = 1; 
+#primer 3.6
+# ẋ = Ax + Bu
+# ẏ = Cx + Du
 
-a11 = -1/C1*(1/R1+1/R2); a12 = -1/(R1*C1); a21 = -1/(R1*C2); a22 = -1/(R1*C2);
-b11 = 1/(R2*C1); b12=0; b21=0; b22=0;
-c11 = -1; c12 = -1; c21=0; c22=1;
-d11 = 1; d12=0; d21=0; d22=0;
-u1 = zeros(size(t)); u2 = zeros(size(t)); u1 = sin(t);
-U = [u1; u2];
-A = [a11, a12; a21, a22]; % osnovna matrika sistema
-B = [b11, b12; b21, b22]; % vhodna matrika
-C = [c11, c12; c21, c22]; % izhodna matrika
-D = [d11, d12; d21, d22];
-x01 = 1; x02 = 0;                    % začetno stanje
-X0 = [x01; x02]; 
+#  | dil1/dt|   |-R1/L  0   -1/L  |   |il1|   |1/L1|
+#  | dil2/dt| = |0    -R2/L2  1/L2| * |il2| + | 0  |Vg
+#  | dVc/dt |   |1/C  -1/C    0   |   |vc |   | 0  |
+#       ẋ     =            A            X       B    u
 
-T = 0:0.01:2; 
-size(T)
-size(U)                 % simulation time = 10 seconds
-#U = 0.1*sin(5*T+1);             % input as a function of time
-sys = sys2ss(A,B,C,D);              % construct a system model
-[Y, Tsim, X] = lsim(A,B,C,D,U,T);   % simulate
+#   |Vr1|    |R1*il1 |   |R1 0 0 |   |il1|   |0|
+#   |   | =  |       | = |       | * |il2| + | |*Vg
+#   |Vr2|    |R2*il2 |   |0 R2 0 |   |vc |   |0|
+#     ẏ   =                  C         x   +  D   u
+clear all; close all;
+#constante
+R1 = 1;
+R2 = 2;
+L1 = 1;
+L2 = 1;
+C  = 1;
+
+#Matrike
+A = [-R1/L1, 0,    -1/L1; 
+     0,    -R2/L2,  1/L2;
+     1/C,    -1/C,     0];
+B = [1/L1, 0, 0]';        #transpose
+C = [R1,   0,   0;
+     0,    R2,  0];
+D = [0 0]';               #transpose
+
+figure(1)
+T = 0:0.01:10;         % simulation time = 10 seconds
+U = zeros(size(T));    % no input
+X0 = [0.1 0.1 0.1];    % initial conditions of the three states
+sys = ss(A,B,C,D);     % construct a system model
+lsim(sys, U, T, X0)    % simulate and plot the response (the output)
+title('Response to Non-Zero Initial Conditions')
+
+#unit step
+figure(2)
+U = ones(size(T));             % u = 1, a step input
+sys = ss(A,B,C,D);             % construct a system model
+[Y, Tsim, X] = lsim(sys,U,T);  % simulate
+plot(Tsim,Y)                   % plot the output vs. time
+title('Step Response with Zero Initial Conditions')
+
+#sinus input
+figure(3)
+T = 0:0.01:10;                  % simulation time = 10 seconds
+U = 0.1*sin(5*T+1);             % input as a function of time
+sys = ss(A,B,C,D);              % construct a system model
+[Y, Tsim, X] = lsim(sys,U,T);   % simulate
 plot(Tsim,Y)                    % plot the output vs. time
-title('Response to a Sinusoid Input with Zero Initial Conditions');
+title('Response to a Sinusoid Input with Zero Initial Conditions')
