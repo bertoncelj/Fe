1540de82ad434e09cb272ed2a790381ac1af5db6
Tine <tine.bertoncelj@gmail.com>
2017-11-21 11:08:36 +0100

    Dela trajektorija v prostoru stanj, dodati treba casovne poteke

--- Signali/Vaja4/LabVaja1a.m
+++ Signali/Vaja4/LabVaja1a.m
@@ -1,61 +1,66 @@
 clear all; close all;
 
-# constant for DE
-freq = 1000; %Hertz
-Ra = 1000;
-Rb = 2000;
- 
-R1 = 1000; #ugotovljena upornost R1 in R2 da nihasitem
-R2 = 1000; #vprasej profesorja kje so to dobi
-
-#kapacitivnosti
-C = 1/(2 * pi * freq * sqrt(R1*R2));
-C1 = C; 
-C2 = C;
-
 #Casovni vektor
 #tz = 0; tk = 0.01; dt = 0.000001;
-t=0 : 0.000001 : 0.01;
+t=0 : 0.1 : 100;
+
+
+# constant for DE
+C = 1; %HFarad
+L = 1;  %Henry
+R = [0, 0.2, 1.4, 2, 4];  %primer za R3
+%R = 2; %test R, DELETE ME later
 
 # U definition input, both zeros matrixs
-U = [zeros(length(t),1), zeros(length(t),1)];
+U = [ones(length(t),1)];
 
-#Zacetne vrednosti Vc1(0)=1V Vc2(0) = 0V
-Vc01 = 1; Vc02 = 0;
-Vc = [Vc01; Vc02];
+#definirane izhodne matrike namenjene da risemo vec krivulj hkrati
+Vc    = [ones(length(t),5)];
+iL    = [ones(length(t),5)];
+vizh  = [ones(length(t),5)];
 
-# definition of A,B,C,D konstants
-a11 = Rb/Ra/(R2*C1)-1/(R1*C1); a12 = -1/(R2*C1);
-a21 = Rb/Ra/(R2*C2); a22 = -1/(R2*C2);
-b11 = 0; b12 = 0; b21 = 0; b22 = 0;
-c11 = 1 + Rb/Ra; c12 = 0;
-d11 = 0; d12 = 0;
 
-#konstante v prostoru matrix
-A = [a11, a12; a21, a22];
-B = [b11, b12; b21, b22];
-C = [c11, c12];
-D = [d11, d12];
+#Zacetne vrednosti Vc(0)=1V iL(0) = 1A
+Vc_zac = 1; iL_zac = 1;
+V_zac_pogoj = [Vc_zac; iL_zac];
 
-#solving systems
-sys = ss(A,B,C,D);
-[y,t,x]=lsim(sys, U,t,Vc);
 
-#zapis outs
-Vc1 = x(:,1); Vc2 = x(:,2); vizh = y(:,1);
+for i = 1 : 5
+  R(i)
+  A   = [0,      1/C;
+        -1/L,  -R(i)/L];
+  
+  B   = [0,    1/L]';
+  %enotska matrika
+  C_m = [1,0;
+         0,1]; 
+  D   = [0, 0]';
+  
+  #solving systems
+  sys = ss(A,B,C_m,D);
+  [y,t,x]=lsim(sys, U,t,V_zac_pogoj);
+  
+  #zapis outs, ki je da lsim: za vse R-je
+  Vc(:,i) = x(:,1);
+  iL(:,i) = x(:,2);
+  vizh(:,i) = y(:,1);
+endfor
 
 #draw
 fig1 = figure(1);
 set(fig1,'Units','centimeters','Position',[1 4 14 12]);
-plot(Vc1, Vc2); axis([-2.1 2.1 -2.1 2.1]); grid;
-xlabel('{\itv_C}_1 [V] ({\itx}_1)'); ylabel('{\itv_C}_2 [V] ({\itx}_2)');
+plot(Vc(:,1),iL(:,1), Vc(:,2),iL(:,2), Vc(:,3),iL(:,3), Vc(:,4),iL(:,4), Vc(:,5),iL(:,5));
+%%axis([-2.1 2.1 -2.1 2.1]); 
+grid;
+legend("ζ=0","ζ=0.1","ζ=0.7","ζ=1","ζ=2")
+xlabel('{\itv_C} [V] ({\itx}_1)'); ylabel('{\iti_L} [V] ({\itx})');
 title('Trajektorija v prostoru stanj')
-pause(1);
+%pause(1);
+%
+%fig2 = figure(2);
 
-fig2 = figure(2);
-
-set(fig2,'Units','centimeters','Position',[16.5 1.5 20 16]);
-plot(t, Vc1, 'g', t, Vc2, 'b', t, vizh, 'r'); axis([0 0.003 -4.5 4.5]); grid;
-xlabel('{\itt} [s]'); ylabel('{\itv_C}_1 [V], {\itv_C}_2 [V], {\itv_{izh}} [V]');
-title('Časovna odvisnost spremenljivk stanj')
-pause(2);
+%set(fig2,'Units','centimeters','Position',[16.5 1.5 20 16]);
+%plot(t, Vc1, 'g', t, Vc2, 'b', t, vizh, 'r'); axis([0 0.003 -4.5 4.5]); grid;
+%xlabel('{\itt} [s]'); ylabel('{\itv_C}_1 [V], {\itv_C}_2 [V], {\itv_{izh}} [V]');
+%title('Časovna odvisnost spremenljivk stanj')
+%pause(2);
