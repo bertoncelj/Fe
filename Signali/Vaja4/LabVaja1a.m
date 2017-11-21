752b09e8118b935bb3d2b7f54f8bfa21f2160e2d
Tine <tine.bertoncelj@gmail.com>
2017-11-21 11:18:56 +0100

    DONE

--- Signali/Vaja4/LabVaja1a.m
+++ Signali/Vaja4/LabVaja1a.m
@@ -46,7 +46,7 @@ for i = 1 : 5
   vizh(:,i) = y(:,1);
 endfor
 
-#draw
+#draw trajektorija stanj v prostoru
 fig1 = figure(1);
 set(fig1,'Units','centimeters','Position',[1 4 14 12]);
 plot(Vc(:,1),iL(:,1), Vc(:,2),iL(:,2), Vc(:,3),iL(:,3), Vc(:,4),iL(:,4), Vc(:,5),iL(:,5));
@@ -55,12 +55,25 @@ grid;
 legend("ζ=0","ζ=0.1","ζ=0.7","ζ=1","ζ=2")
 xlabel('{\itv_C} [V] ({\itx}_1)'); ylabel('{\iti_L} [V] ({\itx})');
 title('Trajektorija v prostoru stanj')
-%pause(1);
-%
-%fig2 = figure(2);
+pause(1);
 
-%set(fig2,'Units','centimeters','Position',[16.5 1.5 20 16]);
-%plot(t, Vc1, 'g', t, Vc2, 'b', t, vizh, 'r'); axis([0 0.003 -4.5 4.5]); grid;
-%xlabel('{\itt} [s]'); ylabel('{\itv_C}_1 [V], {\itv_C}_2 [V], {\itv_{izh}} [V]');
-%title('Časovna odvisnost spremenljivk stanj')
-%pause(2);
+#draw izhodne spremeljivke kondezator
+fig2 = figure(2);
+set(fig2,'Units','centimeters','Position',[16.5 1.5 20 16]);
+plot(t,Vc(:,1), t,Vc(:,2), t,Vc(:,3), t,Vc(:,4), t,Vc(:,5)); 
+legend("ζ=0","ζ=0.1","ζ=0.7","ζ=1","ζ=2")
+axis([0 16 -0.1 2.1]); 
+grid;
+xlabel('{\itt} [s]'); ylabel('{\itv_C} [V]');
+title('Časovna odvisnost spremenljivk stanj kondezatroja')
+pause(2);
+
+fig3 = figure(3);
+set(fig3,'Units','centimeters','Position',[16.5 1.5 20 16]);
+plot(t,iL(:,1), t,iL(:,2), t,iL(:,3), t,iL(:,4), t,iL(:,5)); 
+legend("ζ=0","ζ=0.1","ζ=0.7","ζ=1","ζ=2")
+axis([0 16 -1 1]); 
+grid;
+xlabel('{\itt} [s]'); ylabel('{\iti_L} [V]');
+title('Časovna odvisnost spremenljivk stanj tuljave')
+pause(2);
