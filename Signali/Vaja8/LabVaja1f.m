f1baf7dcffd34c8a326293134778b4deddd7da79
Tine Bertoncelj <king.tine@gmail.com>
2018-01-26 19:18:05 +0100

    DONE

--- Signali/Vaja8/LabVaja1f.m
+++ Signali/Vaja8/LabVaja1f.m
@@ -1,36 +1,76 @@
 clear all; close all
 #LabVaja1f
+%1.f Za vse primere pod c. narišite časovne odzive na enotino stopnico in jih opišite.
 
-t = 0:0.0000001:0.0001;
+#definiran cas
+  t = 0:0.00000001:0.0001;
 
-#Data:
+#Data za polinom
+G0 = 10^4;
 wp1 = 10^5;
 wp2 = 10^6;
 wp3 = 10^7;
 
- barva = ['k', 'r', 'm', 'b', 'y', 'm'];
-#G(s)
-  %Numerator
-  G0 = 10^4;
+#Za izris grafa nastavljeni vektorji, za vsako beto posebej
+  barva = ['k', 'r', 'm', 'b', 'y', 'm'];
   barve = ['r', 'b', 'g', 'm', 'k']
-  B = [ 0.012210 0.00148  0.2265];
-  %Dominator
+  B = [0 0.012210 0.00148  0.2265];
+  naslov_grafa = ["Brez povratne zanke"; "mejno stabilen"; "Stabilen"; "Nestabilen"];
+  osi_mag = [0, 10000, 180, 900];
+  osi_cas = [0.0001, 2e-05, 6e-05]
+ 
+ 
+  # DRAW GRAPHS
+  # Imamo brez povratne, mejno in stabilen tlele
+  for i = 1:length(B) - 1
+  
+    a3 = 1; 
+    a2 = wp1 + wp2 + wp3;
+    a1 = wp1*wp2 + wp1*wp3 + wp2*wp3;
+    a0 = wp1*wp2*wp3*(1 + B(i)*G0);
+    
+    num = [G0*wp1*wp2*wp3];
+    den = [a3 a2 a1 a0];
+    sys = tf(num, den);
+    y = step(sys, t);
+      
+    fig = figure(i)
+    plot(t, y, barve(i))
+    axis([0 osi_cas(i) osi_mag(1) osi_mag(i+1)])
+    call_string = cellstr(naslov_grafa)
+    title(call_string(i));
+    pause(i);
+  endfor
+  
+  # NESTABILEN
+  # Za nestabilen je posebej graf
+  %   Razlaga: Problem je bil, ko je step func bila racunana je dobu prevlke vrednosti za izris nekej na e^124
+  %   Zato se ga posebej racuna, da se vzame samo nekej 1000 prvih rezultatov in graf omeji njegov casovn raznose
+  
+  # Poracunamo senkrat polinosko enacbo za nestabilen sistem
   a3 = 1; 
   a2 = wp1 + wp2 + wp3;
   a1 = wp1*wp2 + wp1*wp3 + wp2*wp3;
-  a0 = wp1*wp2*wp3;
+  a0 = wp1*wp2*wp3*(1 + B(4)*G0);
   
-  
- 
-  #Draw Bode of G(s)
-  for i = 1:length(B)
-  fig = figure(i)
-  num = [G0*wp1*wp2*wp3*B(i)];
+  num = [G0*wp1*wp2*wp3];
   den = [a3 a2 a1 a0];
   sys = tf(num, den);
-  y = step(sys, t);
-  plot(t,y, barve(i))
-  pause(i)
-  endfor
+  y = step(sys, t); 
+
+  #Vzamemo samo prvih 1000 vrenosti y in t (casa)
+  y = y(1:1000);
+  t = t(1:1000);
   
-  
+  #Zasiftamo y v levo, in mu spredej dodamo nekej null, enako za cas, samo da mu na koncu dodamo enke
+  a = zeros(45,1:45)';
+  ones = ones(45,1:45)';
+  y_shift = vertcat(a', y) ;
+  t_shift = vertcat(t', ones');
+
+  #Izris grafa NESTABILEN
+  figure(4)
+  plot(t_shift, y_shift, barve(4))
+  axis([0 3e-06 -30 30])
+  title(call_string(4));
+  pause(4)
